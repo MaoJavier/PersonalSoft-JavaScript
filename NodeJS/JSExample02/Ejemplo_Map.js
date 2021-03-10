8e2c352913cdab06c7a1b7882a7d048bd8bf1967
Javier Mauricio Ortiz <62898864+MaoJavier@users.noreply.github.com>
2021-03-10 11:06:41 -0500

    Update Ejemplo_Map.js

--- NodeJS/JSExample02/Ejemplo_Map.js
+++ NodeJS/JSExample02/Ejemplo_Map.js
@@ -1,6 +1,6 @@
 
     
-    const facturas =[
+  const facturas =[
         {id:1,precio:30000},
         {id:2,precio:50000},
         {id:3,precio:100000}
@@ -9,4 +9,5 @@
     const descuentoFact=facturas.map(factura=>({
         ...factura,precio:factura.precio*(1-(descuento/100))
     }))
+    console.log("FACTURAS CON DESCUENTO DEL "+descuento+"%");  
     console.log(descuentoFact);  
