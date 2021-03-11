561903f2c15ba82d811ab4ad0ce04b4c6ff2cba5
Javier Mauricio Ortiz <62898864+MaoJavier@users.noreply.github.com>
2021-03-11 16:28:20 -0500

    Add files via upload

--- NodeJS/taller.js
+++ NodeJS/taller.js
@@ -16,46 +16,53 @@ console.log(resultado);
 
 // Programe una expresión regular que de este texto  "'hola tu' tururú" obtenga ["'hola tu'", "'"]
 let cadena = "'hola tu' tururú";
-let expresion = /\u0027{1}|hola tu/gu;
+let expresion = /\'{1}|hola tu/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//[ "'", 'hola tu', "'" ]
 
 // Programe una expresión regular que de este texto   "papa" tururú" obtenga ["papa", "pa"]
 let cadena =  "papa tururú";
-let expresion = /pa/g;
+let expresion = /papa|pa/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//['papa']
 
 // Programe una expresión regular que de este texto  "java es güay" obtenga ["java"]
 let cadena =  "java es güay";
 let expresion = /\w{4}/g;;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//[ 'java' ]
+
 
 // Programe una expresión regular que de este texto  "11 millas 10 km" obtenga ["10"]
 let cadena =  "11 millas 10 km";
 let expresion = /\d{2}(?= km)/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//[ '10' ]
 
 // Programe una expresión regular que de este texto  "11 millas 10 km" obtenga ["10 km"]
 let cadena =  "11 millas 10 km";
 let expresion = /\w+ km/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//[ '10 km' ]
 
 // Programe una expresión regular que de este texto   "11.22" obtenga [".22"]
 let cadena =  "11.22";
 let expresion = /(?<=\.)\d+/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
+//[ '22' ]
 
 // Programe una expresión regular que de este texto    "11 km 12 km 14 m" obtenga ["14"]
 let cadena =  "11 km 12 km 14 m";
 let expresion = /\d+(?= m)/g;
 let resultado = cadena.match(expresion);
 console.log(resultado);
-
+//[ '14' ]
 
 
 
