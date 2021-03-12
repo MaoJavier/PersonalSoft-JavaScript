3feed02b5cd6da781ead2d9c0cb9f4674a48ad0b
Javier Mauricio Ortiz <62898864+MaoJavier@users.noreply.github.com>
2021-03-12 13:49:15 -0500

    Add files via upload

--- NodeJS/Ejercicio_horas.js
+++ NodeJS/Ejercicio_horas.js
@@ -9,52 +9,53 @@
  
  */
 
-let numeroSegmentos;
-resolver(1);
-console.log(numeroSegmentos);
-
-function resolver (segundos)
-{
-while (segundos>=0) {
- let hora_actual= new Date();
- hora_actual = segundosToDate(segundos);
-    for(j = 0; j < 6; j++){
-                console.log("entro");
-                numero = hora_actual.toLocaleTimeString().charAt(j);
-                switch(numero){
-                    case 0: case 6: case 9: numeroSegmentos += 6;break;
-                    case 1: numeroSegmentos += 2;break;
-                    case 2: case 3: case 5: numeroSegmentos += 5;break;
-                    case 4: numeroSegmentos += 4;break;
-                    case 7: numeroSegmentos += 3;break;
-                    case 8: numeroSegmentos += 7;break;
-                }
-    }
-
-
- segundos--;
+ let numeroSegmentos;
+ resolver(1);
+ console.log(numeroSegmentos);
+ 
+ function resolver (segundos)
+ {
+ while (segundos>=0) {
+  hora_actual = segundosToDate(segundos);
+  console.log("hora_actual "+hora_actual);
+  expresion = /[0-9]/g;
+  hora = hora_actual.match(expresion);
+  console.log("hora string "+hora);
+     /*
+     for(j = 0; j < 6; j++){
+                 
+                 numero = hora_actual.toLocaleTimeString().charAt(j);
+                 switch(numero){
+                     case 0: case 6: case 9: numeroSegmentos += 6;break;
+                     case 1: numeroSegmentos += 2;break;
+                     case 2: case 3: case 5: numeroSegmentos += 5;break;
+                     case 4: numeroSegmentos += 4;break;
+                     case 7: numeroSegmentos += 3;break;
+                     case 8: numeroSegmentos += 7;break;
+                 }
+     }*/
+ 
+ 
+  segundos--;
+  }
  }
-}
-
-function segundosToDate(segundos) {
-  let hora= new Date();
-  
-  var hour = Math.floor(segundos / 3600);
-  hour = (hour < 10)? '0' + hour : hour;
-  
-  hora.setHours(hour);
-  
-  var minute = Math.floor((segundos / 60) % 60);
-  minute = (minute < 10)? '0' + minute : minute;
-  
-  hora.setMinutes(minute);
-  
-  
-  var second = segundos % 60;
-  second = (second < 10)? '0' + second : second;
-  
-  hora.setSeconds(second);
-  
-  return hora;
-}
-
+ 
+ function segundosToDate(segundos) {
+   let hora= new Date();
+   var hour = Math.floor(segundos / 3600);
+   hour = (hour < 10)? '0' + hour : hour;
+   hora.setHours(hour);
+   
+   var minute = Math.floor((segundos / 60) % 60);
+   minute = (minute < 10)? '0' + minute : minute;
+   hora.setMinutes(minute);
+   
+   
+   var second = segundos % 60;
+   second = (second < 10)? '0' + second : second;
+   hora.setSeconds(second);
+   
+   return hora.toLocaleTimeString();
+ }
+ 
+ 
